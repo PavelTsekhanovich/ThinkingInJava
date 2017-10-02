64008ad890294a45d04a374e364daec28de4d709
Pavel Tsekhanovich <p.tsekhanovich93@gmail.com>
2017-10-02 14:46:59 +0300

    Control structures.

---
+++ src/part/three/WhileTest.java
@@ -0,0 +1,17 @@
+package part.three;
+
+public class WhileTest {
+
+    static boolean condition(){
+        boolean result = Math.random() < 0.99;
+        System.out.print(result + ", ");
+        return result;
+    }
+
+    public static void main(String[] args){
+        while (condition()){
+            System.out.println("Inside While");
+        }
+        System.out.println("Exited Whlie");
+    }
+}
