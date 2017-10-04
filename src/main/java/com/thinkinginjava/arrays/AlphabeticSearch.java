c136647c4de8fa985de6ff5a6c7545e13703b98e
Pavel Tsekhanovich <p.tsekhanovich93@gmail.com>
2017-10-04 15:28:49 +0300

    Arrays.

---
+++ src/main/java/com/thinkinginjava/arrays/AlphabeticSearch.java
@@ -0,0 +1,19 @@
+package com.thinkinginjava.arrays;//: com.thinkinginjava.arrays/AlphabeticSearch.java
+// Searching with a Comparator.
+
+import com.thinkinginjava.net.mindview.util.Generated;
+import com.thinkinginjava.net.mindview.util.RandomGenerator;
+
+import java.util.Arrays;
+
+public class AlphabeticSearch {
+    public static void main(String[] args) {
+        String[] sa = Generated.array(new String[30],
+                new RandomGenerator.String(5));
+        Arrays.sort(sa, String.CASE_INSENSITIVE_ORDER);
+        System.out.println(Arrays.toString(sa));
+        int index = Arrays.binarySearch(sa, sa[10],
+                String.CASE_INSENSITIVE_ORDER);
+        System.out.println("Index: " + index + "\n" + sa[index]);
+    }
+}
