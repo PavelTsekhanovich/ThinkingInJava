91f311e8d7dae86b8ace890e7d1806262d278e28
Pavel Tsekhanovich <p.tsekhanovich93@gmail.com>
2017-10-04 14:17:44 +0300

    Objects.

---
+++ src/main/java/com/thinkinginjava/object/Documentation3.java
@@ -0,0 +1,14 @@
+package com.thinkinginjava.object;
+
+/**
+ * You can <em>even</em> insert a list:
+ * <ol>
+ * <li> Item one
+ * <li> Item two
+ * <li> Item three
+ * </ol>
+ */
+public class Documentation3 {
+
+}
+
