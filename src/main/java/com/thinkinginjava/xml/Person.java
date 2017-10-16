953705e151790a6799dc682159c174208f14ac59
Pavel Tsekhanovich <p.tsekhanovich93@gmail.com>
2017-10-16 20:23:19 +0300

    Minor fix.

--- src/main/java/com/thinkinginjava/xml/Person.java
+++ src/main/java/com/thinkinginjava/xml/Person.java
@@ -1,4 +1,4 @@
-//: com.thinkinginjava.xml/Person.java
+package com.thinkinginjava.xml;//: com.thinkinginjava.xml/Person.java
 // Use the XOM library to write and read XML
 // {Requires: nu.xom.Node; You must install
 // the XOM library from http://www.xom.nu }
