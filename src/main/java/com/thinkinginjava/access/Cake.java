acfc5feb00b8e501ad97fcee81d179cb1b30badf
Pavel Tsekhanovich <p.tsekhanovich93@gmail.com>
2017-10-23 18:30:16 +0300

    Minor fix.

--- src/main/java/com/thinkinginjava/access/Cake.java
+++ src/main/java/com/thinkinginjava/access/Cake.java
@@ -1,11 +1,8 @@
-package com.thinkinginjava.access;//: access/Cake.java
-// Accesses a class in a separate compilation unit.
+package com.thinkinginjava.access;
 
 class Cake {
     public static void main(String[] args) {
         Pie x = new Pie();
         x.f();
     }
-} /* Output:
-Pie.f()
-*///:~
+}
