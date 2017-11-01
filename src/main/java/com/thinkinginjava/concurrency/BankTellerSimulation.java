c037b89b19193251c5b153b61f8896d703b6ebcf
Pavel Tsekhanovich <p.tsekhanovich93@gmail.com>
2017-11-01 16:09:47 +0300

    Minor fix.

--- src/main/java/com/thinkinginjava/concurrency/BankTellerSimulation.java
+++ src/main/java/com/thinkinginjava/concurrency/BankTellerSimulation.java
@@ -9,7 +9,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-// Read-only objects don't require synchronization:
 class Customer {
     private final int serviceTime;
 
